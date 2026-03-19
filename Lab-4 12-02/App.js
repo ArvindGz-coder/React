90f3d3b6f4c63ad4a19100e65de075879a994d44
ArvindGz-coder <arvind1306gupta@gmail.com>
2026-03-19 08:55:44 +0530

    third commit

---
+++ Lab-4 12-02/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import CourseList from "./CourseList";
+import StudentClass from "./StudentClass";
+import StudentFunctional from "./StudentFunctional";
+
+function App() {
+  const [showcourses, setShowCourses] = useState(false);
+  const [showStudents, setShowStudents] = useState(false);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Mount and Unmount Example</h1>
+      <button onClick={() => setShowCourses(!showcourses)}>
+        Toggle Course List
+      </button>
+      <button onClick={() => setShowStudents(!showStudents)}>
+        Toggle Student List
+      </button>
+
+      <hr />
+
+      {showcourses && <CourseList />}
+      {showStudents && <StudentClass />}
+      <StudentFunctional />
+    </div>
+  );
+}
+
+export default App;
+
