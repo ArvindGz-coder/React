aa19bef205a59ad1a11facb71ed2509edbff9665
ArvindGz-coder <arvind1306@gmail.com>
2026-05-21 01:06:35 +0530

    Replace Lab-7/8/9 gitlinks with actual contents

---
+++ Lab-8 09-04/Lab-4 12-02/StudentClass.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class StudentClass extends Component{
+    componentDidMount(){
+        console.log("StudentClass Component Mounted");
+        
+    }
+    componentWillUnmount(){
+        console.log("StudentClass Component Unmounted");
+        
+    }
+    render(){
+        const students = [
+            {name:"Ali",age:20},
+            {name:"Sara",age:22},
+            {name:"John",age:19}
+        ];
+        return (
+
+            <div>
+                <h3>Students List (Class Component)</h3>
+                <ul>
+                    {students.map((student, index) => (
+                        <li key={index}>
+                            {student.name} - {student.age} years
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+export default StudentClass;
---
+++ Lab-9 16-04/Lab-5 19-02/components/TaskManager.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import InfoCard from "./InfoCard";
+
+function TaskManager() {
+  const [task, setTask] = useState("Complete Raect Lab");
+
+  const updateTask = () => {
+    setTask("Prepare Advanced React Notes");
+  };
+
+  return (
+    <div
+      style={{
+        border: "2px solid #444",
+        padding: "15px",
+        marggin: "10px",
+        borderRadius: "8px",
+        backgroundColor: "#f4f4f4",
+      }}
+    >
+      <h2>Hello</h2>
+      {<InfoCard title="Task Manager" content={`Current Task: ${task}`} />}
+      <button onClick={updateTask}>Change Task</button>
+    </div>
+  );
+}
+
+export default TaskManager;
